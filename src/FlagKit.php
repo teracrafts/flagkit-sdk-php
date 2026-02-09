9626f30efebeb5c483a42277f14abd4ee96988a2
Kingsley Eze <firebox@ikingsley.com>
2026-02-10 00:00:02 +0100

    chore: bump version to 1.0.5

--- src/FlagKit.php
+++ src/FlagKit.php
@@ -36,7 +36,7 @@ use FlagKit\Types\FlagState;
 final class FlagKit
 {
     /** SDK Version */
-    public const VERSION = '1.0.4';
+    public const VERSION = '1.0.5';
 
     private static ?FlagKitClient $instance = null;
 
